a033c8049c7c6652c17003fd302ec01327b21cfb
LiuRivers <zhaoxiang0805@hotmail.com>
2018-05-25 08:59:14 +0800

    rabbitmq quickstart

---
+++ rabbitmq/src/main/java/com/youxiang/mq/quickstart/RabbitConsumer.java
@@ -0,0 +1,47 @@
+package com.youxiang.mq.quickstart;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author: Rivers
+ * @date: 2018/5/25
+ */
+public class RabbitConsumer {
+
+    private static final String QUEUE_NAME = "queue_demo";
+    private static final String IP_ADDRESS = "39.108.141.144";
+    private static final int PORT = 5672;
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+
+        Address[] addresses = new Address[]{new Address(IP_ADDRESS, PORT)};
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("root");
+        factory.setPassword("root");
+        Connection connection = factory.newConnection(addresses);
+        final Channel channel = connection.createChannel();
+        channel.basicQos(64);
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("recv message:" + new String(body));
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                channel.basicAck(envelope.getDeliveryTag(), false);
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, consumer);
+        TimeUnit.SECONDS.sleep(5);
+        channel.close();
+        connection.close();
+    }
+}
